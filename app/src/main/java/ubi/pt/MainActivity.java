78dc015c59b44d438d88ec7c4228c54553411db1
TiagoEsteves07 <tiagoesteves07@gmail.com>
2019-04-16 01:15:30 +0100

    Initial commit

---
+++ app/src/main/java/ubi/pt/MainActivity.java
@@ -0,0 +1,58 @@
+package ubi.pt;
+
+import android.os.Bundle;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.perfil:
+                fragment = new Perfil();
+                break;
+            case R.id.store:
+                fragment = new Store();
+                break;
+            case R.id.map:
+                fragment = new Map();
+                break;
+            case R.id.achiv:
+                fragment = new Achivements();
+                break;
+            case R.id.def:
+                fragment = new Definitions();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+
+
+
+}
